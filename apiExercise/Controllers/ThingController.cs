824294ff9071c1586d63083f611a9e9e7000b004
Julia Ilasova <1julka1il@gmail.com>
2023-08-17 10:35:37 +0200

    basic Get, Post, Put, Delete finished in apiExercise

--- apiExercise/Controllers/ThingController.cs
+++ apiExercise/Controllers/ThingController.cs
@@ -6,14 +6,34 @@ namespace apiExercise.Controllers;
 [Route("controller")]
 public class ThingController : ControllerBase
 {
-    private readonly List<string> _names = new List<string>() { "John", "Jane", "Jack"};
+    private readonly List<Thing> _things = new List<Thing>()
+    {
+        new Thing()
+        {
+            Id = 1,
+            Name = "Thing 1",
+            Age = 10
+        },
+        new Thing()
+        {
+            Id = 2,
+            Name = "Thing 2",
+            Age = 20
+        },
+        new Thing()
+        {
+            Id = 3,
+            Name = "Thing 3",
+            Age = 30
+        }
+    };
     
     [HttpGet("entity/{id}")]
-    public string Get([FromRoute] int id)
+    public Thing Get([FromRoute] int id)
     {
         HttpContext.Response.StatusCode = 212;
         HttpContext.Response.Headers.Add("Custom-Header", "This is a custom header value");
-        return _names[id];
+        return _things.First(thing => thing.Id == id);
     }
 
     /*
@@ -26,11 +46,44 @@ public class ThingController : ControllerBase
     */
     
     [HttpGet]
-    public IActionResult Get([FromHeader] string someKey)
+    public IActionResult Get([FromHeader] string name)
     {
         // Create a custom response header
         HttpContext.Response.Headers.Add("Custom-Header", "This is a custom header value");
         
-        return Ok(_names.IndexOf(someKey));
+        return Ok(_things.First(thing => thing.Name == name).Id);
+    }
+
+    [HttpPost]
+    public IActionResult Create([FromBody] Thing thing)
+    {
+        return Created("http://localhost:5000/thing/1", thing);
+    }
+    
+    [HttpPut("{id}")]
+    public IActionResult Update([FromRoute] int id, [FromBody] Thing thing)
+    {
+        var thingToUpdate = _things.First(thing => thing.Id == id);
+        if (thingToUpdate is null)
+        {
+            return NotFound();
+        }
+        
+        thingToUpdate.Name = thing.Name;
+        thingToUpdate.Age = thing.Age;
+        return Ok(thingToUpdate);
+    }
+    
+    [HttpDelete("{id}")]
+    public IActionResult Delete([FromRoute] int id)
+    {
+        var thingToDelete = _things.First(thing => thing.Id == id);
+        if (thingToDelete is null)
+        {
+            return NotFound();
+        }
+        
+        _things.Remove(thingToDelete);
+        return Ok();
     }
 }
---
+++ apiExercise/Thing.cs
@@ -0,0 +1,8 @@
+namespace apiExercise;
+
+public class Thing
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public int? Age { get; set; }
+}
